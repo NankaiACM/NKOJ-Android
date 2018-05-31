89b481a541c5c30a346fb5585e064af7150d5e4c
Sun Yi-Ran <sunrisefox@qq.com>
2018-05-31 17:19:13 +0900

    添加了关于页面（吃瓜） 因为 Fragment 在新版安卓废弃了，所以统一变为 support.v4

--- main/src/main/java/cn/edu/nankai/onlinejudge/main/AboutFragment.kt
+++ main/src/main/java/cn/edu/nankai/onlinejudge/main/AboutFragment.kt
@@ -1,17 +1,65 @@
 package cn.edu.nankai.onlinejudge.main
 
-
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v4.view.animation.FastOutSlowInInterpolator
+import android.support.v4.view.animation.LinearOutSlowInInterpolator
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 class AboutFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val mImageViewPager = view.findViewById(R.id.pager) as ViewPager
+        mImageViewPager.adapter = ScreenSlidePagerAdapter((activity as AppCompatActivity).supportFragmentManager)
+
+        val tabLayout = view.findViewById(R.id.tabDots) as TabLayout
+        tabLayout.setupWithViewPager(mImageViewPager, true)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val vg = tabLayout.getChildAt(0) as ViewGroup
+                vg.alpha = 0.8f
+                vg.animate()
+                        .alpha(0.4f)
+                        .setStartDelay(600)
+                        .setDuration(400)
+                        .setInterpolator(LinearOutSlowInInterpolator())
+                        .start()
+
+                val vgTab = vg.getChildAt(tab.position) as ViewGroup
+                vgTab.scaleX = 0.8f
+                vgTab.scaleY = 0.8f
+                vgTab.animate()
+                        .scaleX(1f)
+                        .scaleY(1f)
+                        .setInterpolator(FastOutSlowInInterpolator())
+                        .setDuration(450)
+                        .start()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+        return view
+    }
+
+    private inner class ScreenSlidePagerAdapter(supportFragmentManager: FragmentManager) : FragmentStatePagerAdapter(supportFragmentManager) {
+        override fun getItem(position: Int): android.support.v4.app.Fragment {
+            return if (position == 1) PageAboutFoxFragment() else BlankFragment()
+        }
+
+        override fun getCount(): Int {
+            return 2
+        }
     }
 }
